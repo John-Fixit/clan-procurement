2b2a57912d1f830ca9757543343760d324e40ea4
Adeoye John Oluwakayode <johnfixit293@gmail.com>
2025-12-01 08:30:56 +0100

    updated

--- src/components/core/projects/purchaseOrderItems.jsx
+++ src/components/core/projects/purchaseOrderItems.jsx
@@ -105,7 +105,7 @@ const PurchaseOrderItems = (props) => {
             className="bg-white border border-gray-200 rounded-lg transition-shadow"
           >
             {/* Item Header */}
-            <div className="flex justify-between items-center py-2 px-6 border-b border-gray-200 bg-gray-200">
+            <div className="flex justify-between items-center py-2 px-6 border-b border-gray-200 bg-gray-200 rounded-t-lg">
               <h4 className="text-sm font-semibold text-gray-700">
                 Item #{index + 1}
               </h4>
@@ -200,17 +200,23 @@ const PurchaseOrderItems = (props) => {
                   >
                     <FaMinus size={14} />
                   </Button>
-                  <InputNumber
-                    type="number"
+                  <Input
                     min="1"
+                    type={"number"}
                     value={watch(`purchase_order_items.${index}.quantity`)}
-                    onChange={(val) => {
+                    onChange={(e) => {
+                      const val = e.target.value;
+                      if (!Number(val)) return;
                       const value = Math.max(1, Number(val) || 1);
                       setValue(`purchase_order_items.${index}.quantity`, value);
-                      // calculateTotal(index);
                     }}
-                    className="w-20 text-center px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none text-sm"
+                    onKeyDown={(e) => {
+                      if (e.key === "e") {
+                        e.preventDefault();
+                      }
+                    }}
                     placeholder="1"
+                    className="w-20"
                   />
                   <Button
                     isIconOnly
